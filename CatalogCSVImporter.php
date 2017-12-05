b84cbdfed570f2c0a45ea6f15fedb62df5f2044c
Alexander Naumov <me@alexandernaumov.de>
2017-12-05 16:44:38 +0100

    implement csv importer

--- CatalogCSVImporter.php
+++ CatalogCSVImporter.php
@@ -2,9 +2,13 @@
 
 namespace CMImporter;
 
-class CatalogCSVImporter {
+use CatalogManager\Toolkit as Toolkit;
+use CatalogManager\CatalogController as CatalogController;
+
+class CatalogCSVImporter extends CatalogController {
 
 
+    protected $arrData = [];
     protected $strDelimiter;
     protected $objFile = null;
 
@@ -28,9 +32,78 @@ class CatalogCSVImporter {
     }
 
 
+    public function prepareData( $arrMapping = [], $blnIgnoreHeader = false ) {
+
+        if ( !is_array( $arrMapping ) || empty( $arrMapping ) ) return null;
+
+        $arrPosition = 0;
+
+        while ( ( $arrData = fgetcsv( $this->objFile, 0, $this->strDelimiter ) ) !== FALSE ) {
+
+            if ( $blnIgnoreHeader && $arrPosition == 0 ) {
+
+                $arrPosition++;
+                continue;
+            };
+
+            $this->arrData[ $arrPosition ] = [];
+
+            foreach ( $arrData as $intIndex => $strValue ) {
+
+                $arrMap = $arrMapping[ $intIndex ];
+                if ( isset( $arrMap['continue'] ) && $arrMap['continue'] ) continue;
+
+                $strFieldname = $arrMap['column'] ? $arrMap['column'] : $arrMap['head'];
+                $this->arrData[ $arrPosition ][ $strFieldname ] = $this->parseValue( $strValue, $arrMap['type'] );
+            }
+
+            $arrPosition++;
+        }
+    }
+
+
+    public function saveCsvToDatabase( $strTable, $blnClearTable = false ) {
+
+        $this->import( 'Database' );
+
+        if ( $blnClearTable && ( is_array( $this->arrData ) && !empty( $this->arrData ) ) ) {
+
+            $this->Database->prepare( sprintf( 'DELETE FROM %s', $strTable ) )->execute();
+        }
+
+        foreach ( $this->arrData as $arrValue ) {
+
+            $this->Database->prepare( 'INSERT INTO '. $strTable .' %s' )->set( $arrValue )->execute();
+        }
+    }
+
+
     public function close() {
 
         fclose( $this->objFile );
         ini_set( 'auto_detect_line_endings', false );
+    }
+
+
+    protected function parseValue( $strValue, $strType ) {
+
+        switch ( $strType ) {
+
+            case 'text':
+
+                return $strValue;
+
+                break;
+
+            case 'file':
+
+                return $strValue;
+
+            case 'date':
+
+                return $strValue;
+        }
+
+        return !Toolkit::isEmpty( $strValue ) ?: '';
     }
 }
