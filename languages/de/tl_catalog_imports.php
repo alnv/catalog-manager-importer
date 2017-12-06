b73f336d196abe071c17b62246f9b935810f93ff
Alexander Naumov <me@alexandernaumov.de>
2017-12-06 16:17:57 +0100

    add labels

---
+++ languages/de/tl_catalog_imports.php
@@ -0,0 +1,26 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['general_settings'] = 'Allgemeine-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog_imports']['data_type_settings'] = 'Datentyp-Einstellungen';
+$GLOBALS['TL_LANG']['tl_catalog_imports']['csv_settings'] = 'CSV-Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['tablename'] = [ 'Tabelle', 'Bitte wählen Sie eine Tabelle aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['csvFile'] = [ 'CSV-Datei', 'Bitte wählen Sie eine CSV-Datei aus.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['state'] = [ 'Status', 'Hier steht der Status des letzten Importes.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['lastState'] = [ 'Status', 'Hier wird der Status des letzten Importes angezeigt.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['last_import'] = [ 'Letzter Import', 'Hier steht das Datum des letzten Importes.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['name'] = [ 'Bezeichnung', 'Bitte geben Sie eine Bezeichnung des CSV Importer ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['clearTable'] = [ 'Tabelle leeren', 'Vor dem Import werden alle Datensätze der Tabelle gelöscht.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['mapping'] = [ 'Mapping', 'Hier können Sie die CSV Kopfzeilen individuell zuweisen und einstellen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['datimFormat'] = [ 'Datum Format', 'Bitte geben Sie, dass in der CSV Datei verwendetes Datum Format ein.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['delimiter'] = [ 'Trennzeichen', 'Hier können Sie ein Trennzeichen definieren. Standardmässig wird ein Komma verwendet.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['filesFolder'] = [ 'Datei Ordner', 'Bitte wählen Sie einen Datei Ordner, in dem sich Ihre Dateien für den Import befinden.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['useCSVHeader'] = [ 'CSV Kopfzeilen verwenden', 'Hier können Sie die CSV Kopfzeilen verwenden, diese werden automatisch der gleichnamigen Tabellen Spalte zugewiesen.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['mappingHeader'] = [
+
+    'column' => 'Tabellenspalte',
+    'type' => 'Datentyp',
+    'head' => 'CSV-Kopfzeile',
+    'continue' => 'Überspringen'
+];
