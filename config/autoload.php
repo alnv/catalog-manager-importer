ac91719f8e1ec73b48b8d3a569a609b1ee0265b8
Alexander Naumov <me@alexandernaumov.de>
2017-12-06 16:18:39 +0100

    change class names

--- config/autoload.php
+++ config/autoload.php
@@ -6,5 +6,5 @@ ClassLoader::addClasses([
 
     'CMImporter\CatalogCSVImporter' => 'system/modules/catalog-manager-importer/CatalogCSVImporter.php',
     'CMImporter\KeyValueMappingWizard' => 'system/modules/catalog-manager-importer/KeyValueMappingWizard.php',
-    'CMImporter\tl_catalog_importer' => 'system/modules/catalog-manager-importer/classes/tl_catalog_importer.php'
+    'CMImporter\tl_catalog_imports' => 'system/modules/catalog-manager-importer/classes/tl_catalog_imports.php'
 ]);
