7f197065c447fbcda709d137a03f98012fba7609
Alexander Naumov <me@alexandernaumov.de>
2017-12-06 16:18:17 +0100

    add globals

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['BE_MOD']['system']['catalog-manager']['tables'][] = 'tl_catalog_importer';
+$GLOBALS['BE_MOD']['system']['catalog-manager']['tables'][] = 'tl_catalog_imports';
 $GLOBALS['BE_FFL']['keyValueMapping'] = 'CMImporter\KeyValueMappingWizard';
 
 if ( TL_MODE == 'BE' ) {
@@ -9,3 +9,18 @@ if ( TL_MODE == 'BE' ) {
         ? 'system/modules/catalog-manager-importer/assets/key_value_mapping_wizard.css'
         : 'system/modules/catalog-manager-importer/assets/key_value_mapping_wizard.css';
 }
+
+$GLOBALS['CTLG_IMPORT_GLOBALS'] = [];
+
+$GLOBALS['CTLG_IMPORT_GLOBALS']['DATA_TYPES'] = [
+
+    'TEXT',
+    'DATE',
+    'FILE'
+];
+
+$GLOBALS['CTLG_IMPORT_GLOBALS']['STATES'] = [
+
+    '200',
+    '404'
+];
