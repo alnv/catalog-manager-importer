4947fcf2f75e1240b555c72ccace394eb1856a44
Alexander Naumov <me@alexandernaumov.de>
2017-12-07 11:13:33 +0100

    change name

--- dca/tl_catalog.php
+++ dca/tl_catalog.php
@@ -2,9 +2,9 @@
 
 array_insert( $GLOBALS['TL_DCA']['tl_catalog']['list']['global_operations'], 0, [
 
-    'csvImporter' => [
+    'importer' => [
 
-        'label' => &$GLOBALS['TL_LANG']['tl_catalog']['csvImporter'],
+        'label' => &$GLOBALS['TL_LANG']['tl_catalog']['importer'],
         'href' => 'table=tl_catalog_imports',
         'icon' => 'edit.gif'
     ]
