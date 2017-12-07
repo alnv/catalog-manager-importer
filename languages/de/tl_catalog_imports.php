e1938a979d4051bc878e74a9c3a281f693f2a464
Alexander Naumov <me@alexandernaumov.de>
2017-12-07 11:12:49 +0100

    add missing labels

--- languages/de/tl_catalog_imports.php
+++ languages/de/tl_catalog_imports.php
@@ -1,16 +1,25 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_catalog_imports']['new'] = [ 'CSV Import erstellen', 'Hier können Sie ein CSV Import erstellen.' ];
+
 $GLOBALS['TL_LANG']['tl_catalog_imports']['general_settings'] = 'Allgemeine-Einstellungen';
 $GLOBALS['TL_LANG']['tl_catalog_imports']['data_type_settings'] = 'Datentyp-Einstellungen';
 $GLOBALS['TL_LANG']['tl_catalog_imports']['csv_settings'] = 'CSV-Einstellungen';
 
+$GLOBALS['TL_LANG']['tl_catalog_imports']['delete'] = [ 'CSV Import löschen', 'CSV Import mit der ID %s löschen.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['edit'] = [ 'CSV Import bearbeiten', 'CSV Import mit der ID %s bearbeiten.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['copy'] = [ 'CSV Import duplizieren','CSV Import mit der ID %s duplizieren.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['import'] = [ 'CSV Import starten','Hier können Sie den CSV Import starten.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['show'] = [ 'Details des CSV Importes anzeigen', 'Die Details des CSV Importes mit der ID %s anzeigen.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['importConfirm'] = 'Soll der Importer ID %s gestartet werden? Bestehende Dateien können gelöscht werden!';
+
 $GLOBALS['TL_LANG']['tl_catalog_imports']['tablename'] = [ 'Tabelle', 'Bitte wählen Sie eine Tabelle aus.' ];
 $GLOBALS['TL_LANG']['tl_catalog_imports']['csvFile'] = [ 'CSV-Datei', 'Bitte wählen Sie eine CSV-Datei aus.' ];
 $GLOBALS['TL_LANG']['tl_catalog_imports']['state'] = [ 'Status', 'Hier steht der Status des letzten Importes.' ];
-$GLOBALS['TL_LANG']['tl_catalog_imports']['lastState'] = [ 'Status', 'Hier wird der Status des letzten Importes angezeigt.' ];
 $GLOBALS['TL_LANG']['tl_catalog_imports']['last_import'] = [ 'Letzter Import', 'Hier steht das Datum des letzten Importes.' ];
 $GLOBALS['TL_LANG']['tl_catalog_imports']['name'] = [ 'Bezeichnung', 'Bitte geben Sie eine Bezeichnung des CSV Importer ein.' ];
-$GLOBALS['TL_LANG']['tl_catalog_imports']['clearTable'] = [ 'Tabelle leeren', 'Vor dem Import werden alle Datensätze der Tabelle gelöscht.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['clearTable'] = [ 'Tabelle leeren', 'Vor dem Import werden alle Datensätze in der Tabelle gelöscht.' ];
 $GLOBALS['TL_LANG']['tl_catalog_imports']['mapping'] = [ 'Mapping', 'Hier können Sie die CSV Kopfzeilen individuell zuweisen und einstellen.' ];
 $GLOBALS['TL_LANG']['tl_catalog_imports']['datimFormat'] = [ 'Datum Format', 'Bitte geben Sie, dass in der CSV Datei verwendetes Datum Format ein.' ];
 $GLOBALS['TL_LANG']['tl_catalog_imports']['delimiter'] = [ 'Trennzeichen', 'Hier können Sie ein Trennzeichen definieren. Standardmässig wird ein Komma verwendet.' ];
@@ -19,8 +28,15 @@ $GLOBALS['TL_LANG']['tl_catalog_imports']['useCSVHeader'] = [ 'CSV Kopfzeilen ve
 
 $GLOBALS['TL_LANG']['tl_catalog_imports']['mappingHeader'] = [
 
-    'column' => 'Tabellenspalte',
     'type' => 'Datentyp',
     'head' => 'CSV-Kopfzeile',
-    'continue' => 'Überspringen'
+    'continue' => 'Ignorieren',
+    'column' => 'Tabellenspalte',
+];
+
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['stateMessages'] = [
+
+    '200' => 'OK',
+    '500' => 'Fehlgeschlagen'
 ];
---
+++ languages/en/tl_catalog_imports.php
@@ -0,0 +1,42 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['new'] = [ 'Create CSV Import', 'Here you can create a CSV import.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['csv_settings'] = 'CSV Settings';
+$GLOBALS['TL_LANG']['tl_catalog_imports']['general_settings'] = 'General Settings';
+$GLOBALS['TL_LANG']['tl_catalog_imports']['data_type_settings'] = 'Data type Settings';
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['copy'] = [ 'Copy CSV Import','Copy CSV Import ID %s.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['edit'] = [ 'Edit CSV Import', 'Edit CSV Import ID "%s".' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['delete'] = [ 'Delete CSV Import', 'Delete CSV Import ID "%s"' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['import'] = [ 'Start CSV import','Here you can start CSV Import.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['show'] = [ 'Show CSV import', 'Show the details of CSV Import ID "%s".' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['importConfirm'] = 'Should the importer ID %s be started? Existing data can be deleted!';
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['name'] = [ 'Name', 'Please enter a importer name.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['csvFile'] = [ 'CSV file', 'Please select your CSV file.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['tablename'] = [ 'Table', 'Please select your destination table.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['state'] = [ 'Status', 'Here you can see the last import status.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['last_import'] = [ 'Last import', 'Here you can see last import.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['datimFormat'] = [ 'Date format', 'Please enter the format used in the CSV file.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['mapping'] = [ 'Mapping', 'Here you can individually assign and set the CSV headers.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['clearTable'] = [ 'Clear table', 'Before import, all records in the table are deleted.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['delimiter'] = [ 'Delimiter', 'Here you can define a separator. By default, a comma is used.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['filesFolder'] = [ 'Files folder', 'Please select a file folder where your files are for import.' ];
+$GLOBALS['TL_LANG']['tl_catalog_imports']['useCSVHeader'] = [ 'Use CSV header', 'Here you can use the CSV headers, these are automatically assigned to the columns of the same name.' ];
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['mappingHeader'] = [
+
+    'type' => 'Data type',
+    'continue' => 'Ignore',
+    'head' => 'CSV header',
+    'column' => 'Table column',
+];
+
+
+$GLOBALS['TL_LANG']['tl_catalog_imports']['stateMessages'] = [
+
+    '200' => 'OK',
+    '500' => 'Failed'
+];
