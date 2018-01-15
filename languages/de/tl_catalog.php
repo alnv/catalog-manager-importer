a2d0ad2c5341ba394b3bfa60d06a5f1a4d068221
Alexander Naumov <me@alexandernaumov.de>
2018-01-15 16:09:49 +0100

    remove tl_catalog

--- languages/de/tl_catalog.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['tl_catalog']['importer'] = [ 'CSV Importer', 'Hier können Sie zu dem CSV Importer Modul wechseln.' ];
--- languages/en/tl_catalog.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['tl_catalog']['importer'] = [ 'CSV Importer', 'Here you can switch to the CSV Importer module.' ];
