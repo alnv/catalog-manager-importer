ab6ca6c4724c56ec60c02d6facc6b617e9b7750c
Alexander Naumov <me@alexandernaumov.de>
2018-02-01 10:10:31 +0100

    change module name

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -1,3 +1,3 @@
 <?php
 
-$GLOBALS['TL_LANG']['MOD']['catalog-manager-importer'] = [ 'Importer', 'Hier können Sie die Daten für Ihre Kataloge importieren.' ];
+$GLOBALS['TL_LANG']['MOD']['catalog-manager-importer'] = [ 'Import', 'Hier können Sie die Daten für Ihre Kataloge importieren.' ];
